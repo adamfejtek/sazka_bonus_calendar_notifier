7c3018dfdd5593c2abeb42057475a9fc3b7bc1a4
Adam Fejtek <123669776+adamfejtek@users.noreply.github.com>
2025-11-23 23:02:00 +0100

    Used Pydantic types

--- src/config.py
+++ src/config.py
@@ -1,12 +1,13 @@
 from functools import cache
+from pydantic import Field, EmailStr, SecretStr
 from pydantic_settings import BaseSettings
 
 class Config(BaseSettings):
-    sazka_email: str
-    sazka_password: str
+    sazka_email: EmailStr
+    sazka_password: SecretStr
 
-    pushover_user_key: str
-    pushover_api_key: str
+    pushover_user_key: SecretStr
+    pushover_api_token: SecretStr
 
 @cache
 def get_config() -> Config:
