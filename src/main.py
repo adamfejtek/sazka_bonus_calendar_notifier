86cce00528f78e4d0b3e6a3c513970c19dca632a
Adam Fejtek <123669776+adamfejtek@users.noreply.github.com>
2025-11-27 23:23:00 +0100

    Formatting

--- src/main.py
+++ src/main.py
@@ -13,9 +13,7 @@ logger = logging.getLogger("sazka_bonus_calendar_notifier")
 logger.setLevel(logging.INFO)
 logger_handler = logging.StreamHandler()
 logger_handler.setLevel(logging.INFO)
-logger_formatter = logging.Formatter(
-    "%(asctime)s | %(name)s | %(levelname)s | %(message)s"
-)
+logger_formatter = logging.Formatter("%(asctime)s | %(name)s | %(levelname)s | %(message)s")
 logger_handler.setFormatter(logger_formatter)
 logger.addHandler(logger_handler)
 
